e62e681df990c99d649fffe488b5b2a075b063d2
Ajay7605 <ajaykumarshetty539@gmail.com>
2024-07-17 23:20:43 +0530

    sync

---
+++ src/api/index.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+// `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`
+
+
+
+export const getPlacesData = async(type,sw,ne)=>{
+    try{
+        const {data:{data}} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+                params: {
+                  bl_latitude:sw.lat,
+                  tr_latitude:ne.lat,
+                  bl_longitude:sw.lng,
+                  tr_longitude:ne.lng,
+              
+                },
+                headers: {
+                  'x-rapidapi-key': '798202c2aamsh79ad92a54c50695p1ca135jsn821c7d94f5d1',
+                  'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+                }
+              })
+              console.log(data)
+            return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+//   params: {
+//     bl_latitude: '11.847676',
+//     tr_latitude: '12.838442',
+//     bl_longitude: '109.095887',
+//     tr_longitude: '109.149359',
+//     restaurant_tagcategory_standalone: '10591',
+//     restaurant_tagcategory: '10591',
+//     limit: '30',
+//     currency: 'USD',
+//     open_now: 'false',
+//     lunit: 'km',
+//     lang: 'en_US'
+//   },
+//   headers: {
+//     'x-rapidapi-key': '5b1b70b804mshf019a4c53b25d64p1247cfjsnc9400c7655a2',
+//     'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,110 @@
+// import React from 'react';
+// import GoogleMapReact from 'google-map-react';
+// import { Paper,Typography,useMediaQuery } from '@material-ui/core';
+// import locationOutlinedIcon from '@material-ui/icons/Location';
+// import Rating from '@material-ui/lab';
+// import useStyles from './styles';
+
+// const Map = ()=>{
+//     const classes = useStyles();
+//     const isMobile = useMediaQuery('(min-width:600px)');
+//     const coordinates = { lat: 0, lng: 0};
+
+//     return(
+//         <div>
+//             <div className={classes.mapContainer}>
+//                 <GoogleMapReact 
+//                 bootstrapURLKeys={{key:''}}
+//                 defaultCenter={coords}
+//                 center={coordinates}
+//                 defaultZoom={14}
+//                 margin={[50,50,50,50]}
+//                 options={''}
+//                 onChange={''}
+//                 onChildClick={''}>
+
+//                 </GoogleMapReact>
+//             <div/>
+                
+
+            
+//         </div>
+//     );
+// }
+
+// export default Map;
+
+
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+import useStyles from './styles';
+
+const Map = ({setCoordinates,setBounds,coordinates,places,setChildClicked}) => {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery('(min-width:600px)');
+  
+  // AIzaSyAq4JQw5FEQXLi6cP0fdLERcaPSiye2VDo(new2)
+  // AIzaSyBCMG7pm3WRtkYstTqQkf5zLD50mSqYCpQ(old)
+  // AIzaSyC77ir-ECtwk_txTHyXU3mdFPHjdD_Zeik(new1)
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAq4JQw5FEQXLi6cP0fdLERcaPSiye2VDo' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{}} // Provide options object here
+        onChange={(e) => {
+          // console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ne: e.marginBounds.ne,sw:e.marginBounds.sw});
+        }} // Provide onChange callback function here
+        onChildClick={(child) => setChildClicked(child)} // Provide onChildClick callback function here
+      >
+        {/* Add any child components or markers here */}
+
+        {places?.map((place,i)=>(
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key = {i}
+          >
+            {
+              !isDesktop ? (
+                <LocationOnOutlinedIcon color='primary' fontSize='large'/>
+
+              ):(
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant='subtitle2' gutterBottom>
+                    {place.name}
+                    </Typography>
+                    <img 
+                      className={classes.pointer}
+                      src = {place.photo ? place.photo.images.large.url : 'https://www.freepik.com/free-photos-vectors/placeholder-restaurant'}
+                      alt={place.name}
+                    />
+                    <Rating size='small' value={Number(place.rating)} readOnly />
+                </Paper>
+
+              )
+
+            }
+
+          </div>
+
+        ))}
+        
+
+
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
